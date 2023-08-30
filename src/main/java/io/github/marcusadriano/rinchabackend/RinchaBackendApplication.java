c354704668eee72c9308036d83ec7d12fb6d5c70
MarcusAdriano <marcusadriano.pereira@gmail.com>
2023-08-30 15:54:26 -0300

    first commit

---
+++ src/main/java/io/github/marcusadriano/rinchabackend/RinchaBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.marcusadriano.rinchabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RinchaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RinchaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/marcusadriano/rinchabackend/RinchaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marcusadriano.rinchabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RinchaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
